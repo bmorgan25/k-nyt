2dbf79cd992e3184f384636c1d09df00661aedfc
bmorgan25 <biggieb003@gmail.com>
2025-03-10 16:58:11 -0400

    Add keyboard for wordle game

---
+++ src/pages/wordle/components/index.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import styles from "./styles.module.css";
+
+// A simple list of characters for the keyboard layout
+const rows = [
+  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+  ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+  ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Delete"],
+];
+
+interface KeyboardProps {
+  onKeyPress: (key: string) => void;
+}
+export function Keyboard({ onKeyPress }: KeyboardProps) {
+  const [pressedKeys, setPressedKeys] = useState<string[]>([]);
+
+  const handleKeyPress = (key: string) => {
+    onKeyPress(key);
+    setPressedKeys((prev) => [...prev, key]); // Optionally track pressed keys
+  };
+
+  return (
+    <div className={styles["keyboard"]}>
+      {rows.map((row, index) => (
+        <div key={index} className={styles["keyboard-row"]}>
+          {row.map((key) => (
+            <button
+              key={key}
+              className={styles[`keyboard-key`]}
+              onClick={() => handleKeyPress(key)}
+            >
+              {key}
+            </button>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/pages/wordle/pages/index.tsx
@@ -0,0 +1,142 @@
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+import { Keyboard } from "../components";
+import styles from "./styles.module.css";
+
+export function Wordle() {
+  const todayWord = "MOOSE";
+
+  const [guess, setGuess] = useState<string[][]>(
+    Array(6).fill(Array(5).fill(""))
+  );
+
+  const [guessIdx, setGuessIdx] = useState<number>(0);
+  const [currIdx, setCurrIdx] = useState<number>(0);
+  const [colorIdxs, setColorIdxs] = useState<string[][]>(
+    Array(6).fill(Array(5).fill("none"))
+  );
+
+  const checkAnswer = () => {
+    const currGuess = guess[guessIdx].join("").toLocaleUpperCase();
+    let tempWord = todayWord;
+
+    const currColors = [...colorIdxs];
+    currColors[guessIdx] = [...colorIdxs[guessIdx]];
+
+    // set green first
+    currGuess.split("").forEach((char, i) => {
+      if (tempWord[i] === char) {
+        currColors[guessIdx][i] = "green";
+      }
+    });
+
+    // use freq map to make sure not adding yellows more than once
+    const freq: { [key: string]: number } = {};
+    tempWord.split("").forEach((ch) => {
+      if (ch in freq) {
+        freq[ch] += 1;
+      } else {
+        freq[ch] = 1;
+      }
+    });
+
+    // remove green letters
+    tempWord = tempWord
+      .split("")
+      .filter((ch, i) => currColors[guessIdx][i] !== "green")
+      .join("");
+
+    // set yellow letters
+    currGuess.split("").forEach((char, i) => {
+      if (currColors[guessIdx][i] === "green") return;
+      if (tempWord.includes(char)) {
+        if (freq[char] >= 1) {
+          currColors[guessIdx][i] = "yellow";
+          freq[char] -= 1;
+        } else {
+          currColors[guessIdx][i] = "grey";
+        }
+      } else {
+        currColors[guessIdx][i] = "grey";
+      }
+    });
+
+    setColorIdxs(currColors);
+    setGuessIdx(guessIdx + 1);
+    setCurrIdx(0);
+  };
+
+  const handleKeyPress = (key: string) => {
+    if (key === "Enter") {
+      // Handle the enter key logic
+      console.log("Submit guess:", guess);
+      //setGuess('');  // Reset after submission
+      checkAnswer();
+    } else if (key === "Delete") {
+      const newGuess = [...guess];
+      newGuess[guessIdx] = [...guess[guessIdx]];
+
+      // Handle backspace (remove the current character)
+      newGuess[guessIdx][currIdx - 1] = "";
+
+      if (currIdx > 0) {
+        setCurrIdx(currIdx - 1);
+      }
+
+      setGuess(newGuess); // Update the state with the new guess
+    } else {
+      if (currIdx >= 5) {
+        return;
+      }
+      console.log(currIdx);
+
+      const newGuess = [...guess];
+      newGuess[guessIdx] = [...guess[guessIdx]];
+
+      //const inputValue = event.target.value;
+
+      if (key === "" || key.length === 1) {
+        // Handle input (non-backspace)
+        newGuess[guessIdx][currIdx] = key;
+        if (currIdx <= 4) {
+          setCurrIdx(currIdx + 1);
+        }
+      }
+
+      setGuess(newGuess); // Update the state with the new guess
+    }
+  };
+
+  useEffect(() => {
+    console.log(guess);
+  }, [guess]);
+
+  return (
+    <div>
+      {/** Wordle section */}
+      <div id="container" className={styles["container"]}>
+        {guess.map((g, idx) => (
+          <div id={`row-${idx}`} className={styles["row"]}>
+            {g.map((char, jdx) => (
+              <div
+                id={`char-${idx}-${jdx}`}
+                className={clsx("row", {
+                  [styles["char-box-green"]]: colorIdxs[idx][jdx] === "green",
+                  [styles["char-box-yellow"]]: colorIdxs[idx][jdx] === "yellow",
+                  [styles["char-box-grey"]]: colorIdxs[idx][jdx] === "grey",
+                  [styles["char-box"]]: colorIdxs[idx][jdx] === "none",
+                })}
+              >
+                {char}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      {/** Input section */}
+      <div>
+        <Keyboard onKeyPress={handleKeyPress} />
+      </div>
+    </div>
+  );
+}
