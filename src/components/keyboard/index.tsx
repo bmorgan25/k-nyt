1003383083b1b0bc73db9f37b49edeb1af2ee044
bmorgan25 <biggieb003@gmail.com>
2025-03-14 15:30:16 -0400

    Prevent width issues

--- src/components/keyboard/index.tsx
+++ src/components/keyboard/index.tsx
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 const rows = [
   ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
   ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-  ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Delete"],
+  ["Enter", "Z", "X", "C", "V", "B", "N", "M", "←"],
 ];
 
 interface KeyboardProps {
